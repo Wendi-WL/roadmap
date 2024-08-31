678bfe99d6d84ccaaf5f83b8fd944e0d2078bcce
Wendi <liu.wendi@yahoo.ca>
2024-08-31 12:31:58 -0700

    refactor phase to have timeframe property instead of inheritance to streamline GUI usage

--- tests/test_phase.py
+++ tests/test_phase.py
@@ -3,6 +3,7 @@ from models.dates import *
 
 d1 = date(2024, 2, 3)
 d2 = date(2024, 3, 4)
+d3 = date(2024, 3, 2)
 
 @pytest.fixture
 def p():
@@ -10,6 +11,8 @@ def p():
 
 def test_constructor(p):
     assert p.name == "Phase 1"
+    assert p.timeframe.start == d1
+    assert p.timeframe.end == d2
 
 def test_constructor_typeerror():
     with pytest.raises(TypeError) as excinfo:  
@@ -30,3 +33,13 @@ def test_set_name_typeerror(p):
     with pytest.raises(TypeError) as excinfo:  
         p.name = True
     assert str(excinfo.value) == "Name should be a string"
+
+def test_set_timeframe(p):
+    tf = Timeframe(d1, d3)
+    p.timeframe = tf
+    assert p.timeframe == tf
+
+def test_set_timeframe_typeerror(p):
+    with pytest.raises(TypeError) as excinfo:  
+        p.timeframe = d1
+    assert str(excinfo.value) == "Timeframe should be a Timeframe"
